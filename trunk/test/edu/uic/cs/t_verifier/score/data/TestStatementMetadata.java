483ff49b5027b93678a2ca10f1d9bfdf79742d47
wanghong.nanjing@gmail.com <wanghong.nanjing@gmail.com@3affad74-fe9c-c19b-1214-63350d2b5f3c>
2011-09-10 17:41:36 +0000

    Switched to the latest AU list got from Binghamton Weight Year type AU by classify them into: year[3], decade[2], century[1]

--- trunk/test/edu/uic/cs/t_verifier/score/data/TestStatementMetadata.java
+++ trunk/test/edu/uic/cs/t_verifier/score/data/TestStatementMetadata.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.TreeSet;
 
 import edu.uic.cs.t_verifier.EnhancedTestCase;
+import edu.uic.cs.t_verifier.common.StatementType;
 
 public class TestStatementMetadata extends EnhancedTestCase
 {
@@ -94,6 +95,114 @@ public class TestStatementMetadata extends EnhancedTestCase
 		TreeSet<String> expected = new TreeSet<String>(
 				Arrays.asList(new String[] { "a[1]", "b[1]", "c[1]", "a b[2]",
 						"b a[2]", "a b c[3]", "b c d[2]", "a b c d[4]" }));
+
+		assertEquals(expected, actual);
+	}
+
+	public void testAssignWeightToAUs_4()
+	{
+		List<String> alternativeUnits = new ArrayList<String>();
+		alternativeUnits.add("1964");
+		alternativeUnits.add("1550");
+		alternativeUnits.add("1980");
+		alternativeUnits.add("1996");
+		alternativeUnits.add("1960");
+
+		StatementMetadata statement = new StatementMetadata(0, null,
+				alternativeUnits.toArray(new String[alternativeUnits.size()]),
+				null, StatementType.YEAR);
+
+		TreeSet<String> actual = new TreeSet<String>();
+		for (AlternativeUnit au : statement.getAlternativeUnits())
+		{
+			System.out.println(au.toString());
+			actual.add(au.toString());
+		}
+
+		TreeSet<String> expected = new TreeSet<String>(
+				Arrays.asList(new String[] { "1964[1]", "1550[1]", "1980[1]",
+						"1996[1]", "1960[1]" }));
+
+		assertEquals(expected, actual);
+	}
+
+	public void testAssignWeightToAUs_5()
+	{
+		List<String> alternativeUnits = new ArrayList<String>();
+		alternativeUnits.add("1964");
+		alternativeUnits.add("1550");
+		alternativeUnits.add("1980");
+		alternativeUnits.add("1996");
+		alternativeUnits.add("1960s");
+
+		StatementMetadata statement = new StatementMetadata(0, null,
+				alternativeUnits.toArray(new String[alternativeUnits.size()]),
+				null, StatementType.YEAR);
+
+		TreeSet<String> actual = new TreeSet<String>();
+		for (AlternativeUnit au : statement.getAlternativeUnits())
+		{
+			System.out.println(au.toString());
+			actual.add(au.toString());
+		}
+
+		TreeSet<String> expected = new TreeSet<String>(
+				Arrays.asList(new String[] { "1964[2]", "1550[2]", "1980[2]",
+						"1996[2]", "1960s[1]" }));
+
+		assertEquals(expected, actual);
+	}
+
+	public void testAssignWeightToAUs_6()
+	{
+		List<String> alternativeUnits = new ArrayList<String>();
+		alternativeUnits.add("1964");
+		alternativeUnits.add("1550");
+		alternativeUnits.add("1980");
+		alternativeUnits.add("1996");
+		alternativeUnits.add("1900s");
+
+		StatementMetadata statement = new StatementMetadata(0, null,
+				alternativeUnits.toArray(new String[alternativeUnits.size()]),
+				null, StatementType.YEAR);
+
+		TreeSet<String> actual = new TreeSet<String>();
+		for (AlternativeUnit au : statement.getAlternativeUnits())
+		{
+			System.out.println(au.toString());
+			actual.add(au.toString());
+		}
+
+		TreeSet<String> expected = new TreeSet<String>(
+				Arrays.asList(new String[] { "1964[2]", "1550[2]", "1980[2]",
+						"1996[2]", "1900s[1]" }));
+
+		assertEquals(expected, actual);
+	}
+
+	public void testAssignWeightToAUs_7()
+	{
+		List<String> alternativeUnits = new ArrayList<String>();
+		alternativeUnits.add("1964");
+		alternativeUnits.add("1550");
+		alternativeUnits.add("1990");
+		alternativeUnits.add("1990s");
+		alternativeUnits.add("1900s");
+
+		StatementMetadata statement = new StatementMetadata(0, null,
+				alternativeUnits.toArray(new String[alternativeUnits.size()]),
+				null, StatementType.YEAR);
+
+		TreeSet<String> actual = new TreeSet<String>();
+		for (AlternativeUnit au : statement.getAlternativeUnits())
+		{
+			System.out.println(au.toString());
+			actual.add(au.toString());
+		}
+
+		TreeSet<String> expected = new TreeSet<String>(
+				Arrays.asList(new String[] { "1964[3]", "1550[3]", "1990[3]",
+						"1990s[2]", "1900s[1]" }));
 
 		assertEquals(expected, actual);
 	}
