c5edcb5a909e5c3170f16067fca084c1d31d58da
wanghong.nanjing@gmail.com <wanghong.nanjing@gmail.com@3affad74-fe9c-c19b-1214-63350d2b5f3c>
2011-09-10 04:55:26 +0000

    remove backup folder "copy"

--- src/edu/uic/cs/t_verifier/score/data/StatementMetadata.java
+++ src/edu/uic/cs/t_verifier/score/data/StatementMetadata.java
@@ -1,5 +1,11 @@
 package edu.uic.cs.t_verifier.score.data;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import edu.uic.cs.t_verifier.common.StatementType;
 
 public class StatementMetadata
@@ -31,9 +37,65 @@ public class StatementMetadata
 		return stemmedNonstopTUWords;
 	}
 
-	public String[] getAlternativeUnits()
+	public List<AlternativeUnit> getAlternativeUnits()
 	{
-		return alternativeUnits;
+		List<AlternativeUnit> result = new ArrayList<AlternativeUnit>(
+				alternativeUnits.length);
+		for (String auString : alternativeUnits)
+		{
+			result.add(new AlternativeUnit(auString));
+		}
+
+		assignWeightToAUs(result);
+
+		return result;
+	}
+
+	private void assignWeightToAUs(List<AlternativeUnit> aus)
+	{
+		Collections.sort(aus, new Comparator<AlternativeUnit>()
+		{
+			@Override
+			public int compare(AlternativeUnit au1, AlternativeUnit au2)
+			{
+				return au1.getWords().length - au2.getWords().length;
+			}
+		});
+
+		////////////////////////////////////////////////////////////////////////
+		int initialLength = aus.get(0).getWords().length;
+		for (int index = 0; index < aus.size(); index++)
+		{
+			AlternativeUnit au_current = aus.get(index);
+			if (au_current.getWords().length == initialLength)
+			{
+				au_current.setWeight(1);
+				continue;
+			}
+
+			AlternativeUnit au_previous = null;
+			for (int j = 0; j < index; j++)
+			{
+				au_previous = aus.get(j);
+				if (au_current.getWords().length == au_previous.getWords().length)
+				{
+					continue;
+				}
+
+				if (Arrays.asList(au_current.getWords()).containsAll(
+						Arrays.asList(au_previous.getWords())))
+				{
+					if (au_current.getWeight() <= au_previous.getWeight())
+					{
+						au_current.setWeight(au_previous.getWeight() + 1);
+					}
+				}
+				else if (au_current.getWeight() < au_previous.getWeight())
+				{
+					au_current.setWeight(au_previous.getWeight());
+				}
+			}
+		}
 	}
 
 	public String[] getMatchedSubTopicUnits()
