92bc077f9e2bfe6d67d40451f1c6327bf36b8abf
wanghong.nanjing@gmail.com <wanghong.nanjing@gmail.com@3affad74-fe9c-c19b-1214-63350d2b5f3c>
2012-02-11 20:34:21 +0000

    If we can't find subject corresponding to the AU, we try objects

--- trunk/src/edu/uic/cs/t_verifier/nlp/impl/NLPAnalyzerImpl2.java
+++ trunk/src/edu/uic/cs/t_verifier/nlp/impl/NLPAnalyzerImpl2.java
@@ -1,7 +1,12 @@
 package edu.uic.cs.t_verifier.nlp.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.commons.lang.StringUtils;
 
 import edu.stanford.nlp.ling.CoreAnnotations.IndexAnnotation;
 import edu.stanford.nlp.ling.TaggedWord;
@@ -16,6 +21,7 @@ import edu.stanford.nlp.trees.TreebankLanguagePack;
 import edu.stanford.nlp.trees.TypedDependency;
 import edu.uic.cs.t_verifier.input.AlternativeUnitsReader;
 import edu.uic.cs.t_verifier.input.data.Statement;
+import edu.uic.cs.t_verifier.misc.Assert;
 import edu.uic.cs.t_verifier.nlp.NLPAnalyzer;
 
 public class NLPAnalyzerImpl2 implements NLPAnalyzer
@@ -43,23 +49,143 @@ public class NLPAnalyzerImpl2 implements NLPAnalyzer
 	{
 		String sentence = statement.getAllAlternativeStatements().get(0);
 		String alternativeUint = statement.getAlternativeUnits().get(0);
+
+		return retrieveTopicTermIfSameTypeAsAU(sentence, alternativeUint);
+	}
+
+	public String retrieveTopicTermIfSameTypeAsAU(String sentence,
+			String alternativeUint)
+	{
+		boolean doubleCheckSubject = true;
 		if (alternativeUint.contains(" ")) // if AU contains more than one term, use "AU" instead. 
 		{
 			sentence = sentence.replace(alternativeUint, AU_SUBSTITUTE);
 			alternativeUint = AU_SUBSTITUTE.toLowerCase(Locale.US);
+			doubleCheckSubject = false;
 		}
 
-		char[] chars = sentence.toCharArray();
-		chars[0] = Character.toUpperCase(chars[0]);
-		sentence = new String(chars);
+		sentence = StringUtils.capitalize(sentence);
 
+		return retrieveTopicTermIfSameTypeAsAUInternal(sentence,
+				alternativeUint, doubleCheckSubject);
+	}
+
+	private String retrieveTopicTermIfSameTypeAsAUInternal(String sentence,
+			String alternativeUint, boolean doubleCheckSubject)
+	{
 		Tree parsedTree = lexicalizedParser.apply(sentence);
 		GrammaticalStructure gs = grammaticalStructureFactory
 				.newGrammaticalStructure(parsedTree);
-		List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+		Collection<TypedDependency> typedDependencyList = gs
+				.typedDependencies();
 
+		AtomicBoolean isPassive = new AtomicBoolean(false);
+		String subjectTerm = retrieveSubjectTerm(alternativeUint, parsedTree,
+				typedDependencyList, isPassive);
+		if (subjectTerm == null)
+		{
+			if (doubleCheckSubject) // check it again. This time, we use AU to replace single AU term. 
+			{
+				sentence = sentence.replace(alternativeUint,
+						StringUtils.capitalize(alternativeUint));
+				subjectTerm = retrieveTopicTermIfSameTypeAsAUInternal(sentence,
+						alternativeUint, false);
+			}
+
+			if (subjectTerm == null && isPassive.get())
+			{
+				String objectTerm = retrieveObjectTerm(alternativeUint,
+						parsedTree, typedDependencyList);
+
+				return objectTerm;
+			}
+		}
+
+		return subjectTerm;
+	}
+
+	private String retrieveObjectTerm(String alternativeUint, Tree parsedTree,
+			Collection<TypedDependency> typedDependencyList)
+	{
+		List<TreeGraphNode> allPossibleObjects = retrieveObjectTermInternal(typedDependencyList);
+		if (allPossibleObjects.isEmpty())
+		{
+			return null;
+		}
+
+		String auTag = null;
+		int auIndex = -1;
+		List<TaggedWord> taggedWords = parsedTree.taggedYield();
+		// find AU
+		for (int index = 0; index < taggedWords.size(); index++)
+		{
+			TaggedWord taggedWord = taggedWords.get(index);
+			String term = taggedWord.word().toLowerCase(Locale.US);
+			if (term.equals(alternativeUint))
+			{
+				auTag = mapPosTagToBasicForm(taggedWord.tag());
+				auIndex = index;
+				break;
+			}
+		}
+
+		Assert.notNull(auTag);
+
+		// String objectTerm = null;
+		// int smallestDiff = Integer.MAX_VALUE;
+		for (TreeGraphNode objectNode : allPossibleObjects)
+		{
+			int index = objectNode.label().get(IndexAnnotation.class)
+					.intValue() - 1;
+			TaggedWord object = taggedWords.get(index);
+
+			/*int indexDiff = Math.abs(auIndex - index);
+			if (indexDiff != 0 && indexDiff < smallestDiff
+					&& auTag.equals(mapPosTagToBasicForm(object.tag())))
+			{
+				objectTerm = objectNode.toString("value")
+						.toLowerCase(Locale.US);
+				smallestDiff = indexDiff;
+			}*/
+
+			if (auIndex != index
+					&& auTag.equals(mapPosTagToBasicForm(object.tag())))
+			{
+				// return the first one matched
+				return objectNode.toString("value").toLowerCase(Locale.US);
+			}
+		}
+
+		// return objectTerm;
+		return null;
+	}
+
+	private List<TreeGraphNode> retrieveObjectTermInternal(
+			Collection<TypedDependency> typedDependencyList)
+	{
+		List<TreeGraphNode> result = new ArrayList<TreeGraphNode>();
+
+		for (TypedDependency typedDependency : typedDependencyList)
+		{
+			GrammaticalRelation grammaticalRelation = typedDependency.reln();
+			String shortName = grammaticalRelation.getShortName();
+			if (shortName.equals("dobj") /*|| shortName.equals("iobj")*/
+					|| shortName.equals("pobj"))
+			{
+				TreeGraphNode dep = typedDependency.dep();
+				result.add(dep);
+			}
+		}
+
+		return result;
+	}
+
+	private String retrieveSubjectTerm(String alternativeUint, Tree parsedTree,
+			Collection<TypedDependency> typedDependencyList,
+			AtomicBoolean isPassive)
+	{
 		TreeGraphNode[] auAndCounterpart = retrieveSubjectInternal(
-				alternativeUint, tdl);
+				alternativeUint, typedDependencyList, isPassive);
 		if (auAndCounterpart == null)
 		{
 			return null;
@@ -68,17 +194,7 @@ public class NLPAnalyzerImpl2 implements NLPAnalyzer
 		TreeGraphNode auNode = auAndCounterpart[0];
 		TreeGraphNode subjectNode = auAndCounterpart[1];
 
-		Integer auNodeIndex = auNode.label().get(IndexAnnotation.class);
-		Integer subjectNodeIndex = subjectNode.label().get(
-				IndexAnnotation.class);
-
-		List<TaggedWord> taggedWords = parsedTree.taggedYield();
-		TaggedWord auTaggedWord = taggedWords.get(auNodeIndex.intValue() - 1);
-		TaggedWord subjectTaggedWord = taggedWords.get(subjectNodeIndex
-				.intValue() - 1);
-		String auTag = mapPosTagToBasicForm(auTaggedWord.tag());
-		String subjectTag = mapPosTagToBasicForm(subjectTaggedWord.tag());
-		if (auTag.equals(subjectTag))
+		if (haveSamePosTag(auNode, subjectNode, parsedTree))
 		{
 			return subjectNode.toString("value").toLowerCase(Locale.US);
 		}
@@ -86,18 +202,44 @@ public class NLPAnalyzerImpl2 implements NLPAnalyzer
 		return null;
 	}
 
+	private boolean haveSamePosTag(TreeGraphNode oneNode,
+			TreeGraphNode otherNode, Tree parsedTree)
+	{
+		Integer oneIndex = oneNode.label().get(IndexAnnotation.class);
+		Integer otherIndex = otherNode.label().get(IndexAnnotation.class);
+
+		List<TaggedWord> taggedWords = parsedTree.taggedYield();
+
+		TaggedWord oneTaggedWord = taggedWords.get(oneIndex.intValue() - 1);
+		TaggedWord otherTaggedWord = taggedWords.get(otherIndex.intValue() - 1);
+
+		String auTag = mapPosTagToBasicForm(oneTaggedWord.tag());
+		String subjectTag = mapPosTagToBasicForm(otherTaggedWord.tag());
+
+		return auTag.equals(subjectTag);
+	}
+
 	/**
 	 * TreeGraphNode[] {AU, counterpart}
 	 */
 	private TreeGraphNode[] retrieveSubjectInternal(String alternativeUint,
-			List<TypedDependency> tdl)
+			Collection<TypedDependency> tdl, AtomicBoolean isPassive)
 	{
+		TreeGraphNode[] result = null;
+
 		for (TypedDependency typedDependency : tdl)
 		{
 			GrammaticalRelation grammaticalRelation = typedDependency.reln();
-			if (grammaticalRelation.getShortName().equals("nsubj")
-					|| grammaticalRelation.getShortName().equals("nsubjpass"))
+			String shortName = grammaticalRelation.getShortName();
+			boolean isNsubj = shortName.equals("nsubj");
+			boolean isNsubjpass = shortName.equals("nsubjpass");
+			if (isNsubj || isNsubjpass)
 			{
+				Assert.isTrue(result == null,
+						"Only one subject related to AU in the sentecne. ");
+
+				isPassive.getAndSet(isNsubjpass);
+
 				TreeGraphNode gov = typedDependency.gov();
 				TreeGraphNode dep = typedDependency.dep();
 
@@ -106,18 +248,19 @@ public class NLPAnalyzerImpl2 implements NLPAnalyzer
 
 				if (alternativeUint.equals(govValue))
 				{
-					return new TreeGraphNode[] { gov, dep };
+					result = new TreeGraphNode[] { gov, dep };
+				}
+				else if (alternativeUint.equals(depValue))
+				{
+					result = new TreeGraphNode[] { dep, gov };
 				}
 
-				if (alternativeUint.equals(depValue))
-				{
-					return new TreeGraphNode[] { dep, gov };
-				}
+				// System.out.println("\t" + shortName + ": " + depValue);
 
 			}
 		}
 
-		return null;
+		return result;
 	}
 
 	private String mapPosTagToBasicForm(String posTag)
@@ -166,11 +309,25 @@ public class NLPAnalyzerImpl2 implements NLPAnalyzer
 		NLPAnalyzerImpl2 analyzer = new NLPAnalyzerImpl2();
 		for (Statement statement : statements)
 		{
-			System.out.print(statement.getId() + "\t");
-			/*System.out.println(analyzer.retrieveSubject(statement));*/
-			System.out.println("["
-					+ analyzer.retrieveTopicTermIfSameTypeAsAU(statement) + "]\t"
+			/*System.out.println(statement.getId() + "\t"
 					+ statement.getAllAlternativeStatements().get(0));
+			
+			analyzer.retrieveTopicTermIfSameTypeAsAU(statement);
+			System.out.println();*/
+
+			System.out.print(statement.getId() + "\t");
+			System.out.println("["
+					+ analyzer.retrieveTopicTermIfSameTypeAsAU(statement)
+					+ "]\t" + statement.getAllAlternativeStatements().get(0));
 		}
+
+		System.out.println(analyzer.retrieveTopicTermIfSameTypeAsAU(
+				"poseidon is known as the greek god of sea", "poseidon"));
+
+		System.out
+				.println(analyzer
+						.retrieveTopicTermIfSameTypeAsAU(
+								"ronald reagan is known as the first private citizen to fly in space",
+								"ronald reagan"));
 	}
 }
