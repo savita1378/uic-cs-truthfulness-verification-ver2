eb93186e4fed0c7038c51c01c4a7f54fa8eb31b4
wanghong.nanjing@gmail.com <wanghong.nanjing@gmail.com@3affad74-fe9c-c19b-1214-63350d2b5f3c>
2012-09-14 05:04:21 +0000

    Hybrid method reaches:
    TREC QA.08.09 ================
    Total accuracy: 5629/5926=0.950
    Non-lower case Precision: 1096/1202=0.912  Recall: 1096/1287=0.852
    Lower case Precision: 4533/4724=0.960  Recall: 4533/4639=0.977
    
    TREC Robust.04 ================
    Total accuracy: 3489/3555=0.981
    Non-lower case Precision: 234/258=0.907  Recall: 234/276=0.848
    Lower case Precision: 3255/3297=0.987  Recall: 3255/3279=0.993

--- src/edu/uic/cs/t_verifier/nlp/impl/HybridTrueCaserImpl.java
+++ src/edu/uic/cs/t_verifier/nlp/impl/HybridTrueCaserImpl.java
@@ -1,8 +1,10 @@
 package edu.uic.cs.t_verifier.nlp.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
 import java.util.Properties;
 
@@ -11,9 +13,14 @@ import opennlp.tools.util.Span;
 import org.apache.commons.lang.WordUtils;
 import org.apache.log4j.Logger;
 
+import edu.mit.jwi.item.POS;
+import edu.stanford.nlp.ling.CoreAnnotations.TrueCaseTextAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.uic.cs.t_verifier.input.AlternativeUnitsReader;
-import edu.uic.cs.t_verifier.input.data.Statement;
 import edu.uic.cs.t_verifier.misc.LogHelper;
 import edu.uic.cs.t_verifier.nlp.impl.OpenNLPChunker.ChunkType;
 
@@ -22,67 +29,7 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 	private static final Logger LOGGER = LogHelper
 			.getLogger(HybridTrueCaserImpl.class);
 
-	private StanfordCoreNLP stanfordTrueCaser;
-
-	public static void main(String[] args)
-	{
-		List<Statement> statements = AlternativeUnitsReader
-				.parseAllStatementsFromInputFiles();
-		HybridTrueCaserImpl impl = new HybridTrueCaserImpl();
-
-		try
-		{
-			for (Statement statement : statements)
-			{
-				//				if (statement.getId().intValue() != 4)
-				//				{
-				//					continue;
-				//				}
-				List<String> allAlternativeUnits = statement
-						.getAlternativeUnits();
-				List<String> allAlternativeStatements = statement
-						.getAllAlternativeStatements();
-
-				//			String alternativeUnit = allAlternativeUnits.get(0);
-				//			String sentence = allAlternativeStatements.get(0);
-				//
-				//			System.out.println("["
-				//					+ statement.getId()
-				//					+ "] "
-				//					+ sentence.replace(alternativeUnit, "[" + alternativeUnit
-				//							+ "]"));
-				//
-				//			String capitalizedSentence = impl.capitalizeProperNounTerms(
-				//					sentence, null);
-
-				for (int index = 0; index < allAlternativeStatements.size(); index++)
-				{
-					String alternativeUnit = allAlternativeUnits.get(index);
-					String sentence = allAlternativeStatements.get(index);
-
-					String originalSentence = "["
-							+ statement.getId()
-							+ "] "
-							+ sentence.replace(alternativeUnit, "["
-									+ alternativeUnit + "]");
-					// System.out.println(originalSentence);
-					LOGGER.info(originalSentence);
-
-					String capitalizedSentence = impl.restoreCases(sentence);
-					System.out.println(/*"> " + */capitalizedSentence);
-
-					LOGGER.info("");
-				}
-				System.out.println();
-
-			}
-		}
-		finally
-		{
-			impl.commitCache(); // DO NOT forget this!
-		}
-
-	}
+	private static StanfordCoreNLP stanfordTrueCaser;
 
 	public HybridTrueCaserImpl()
 	{
@@ -116,22 +63,26 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 		sentence = sentence.trim();
 		sentence = sentence.replaceAll("\\s{2,}", " ");
 
-		List<Entry<String, String>> posTagsByTerm = parseIntoPosTagByTerms(
-				sentence, false); // this one is not in basic form!!
+		List<Entry<String, String>> posTagsByOriginalCase = new ArrayList<Entry<String, String>>();
+		List<Entry<String, String>> posTagsByTrueCase = new ArrayList<Entry<String, String>>();
+		parseIntoPosTagByOriginalAndTrueCase(sentence, posTagsByOriginalCase,
+				posTagsByTrueCase); // this one is not in basic form!!
+
 		// split by the punctuation
-		List<List<Entry<String, String>>> originalPosTagsByTermOfSubSentences = splitByPunctuations(posTagsByTerm); // original form for chunking
+		List<List<Entry<String, String>>> originalPosTagsByOriginalCaseOfSubSentences = splitByPunctuations(posTagsByOriginalCase); // original form for chunking
+		List<List<Entry<String, String>>> originalPosTagsByTrueCaseOfSubSentences = splitByPunctuations(posTagsByTrueCase);
 
 		List<List<Entry<String, String>>> allNounPhrases = new ArrayList<List<Entry<String, String>>>();
-		List<List<Entry<String, String>>> posTagsByTermOfSubSentences = new ArrayList<List<Entry<String, String>>>(
-				originalPosTagsByTermOfSubSentences.size());
+		List<List<Entry<String, String>>> posTagsByOriginalCaseOfSubSentences = new ArrayList<List<Entry<String, String>>>(
+				originalPosTagsByOriginalCaseOfSubSentences.size());
 
 		List<String> matchedAcronyms = new ArrayList<String>();
 		List<Entry<List<Entry<String, String>>, String>> matchedFullNames = new ArrayList<Entry<List<Entry<String, String>>, String>>();
 
-		for (List<Entry<String, String>> tagsByTermOriginalForm : originalPosTagsByTermOfSubSentences) // for each sub-sentence
+		for (List<Entry<String, String>> tagsByTermOriginalForm : originalPosTagsByOriginalCaseOfSubSentences) // for each sub-sentence
 		{
 			List<Entry<String, String>> tagsByTermBasicForm = mapPosTagToBasicForm(tagsByTermOriginalForm);
-			posTagsByTermOfSubSentences.add(tagsByTermBasicForm); // basic form
+			posTagsByOriginalCaseOfSubSentences.add(tagsByTermBasicForm); // basic form
 
 			String acronym = searchForAcronyms(tagsByTermBasicForm);
 			matchedAcronyms.add(acronym);
@@ -146,27 +97,6 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 						.subList(span.getStart(), span.getEnd());
 				List<Entry<String, String>> phraseBasicForm = mapPosTagToBasicForm(phraseOriginalForm); // basic form
 				allNounPhrases.add(phraseBasicForm);
-
-				//				Entry<String, String> tagsByTermBeforeNP = (span.getStart() == 0) ? null
-				//						: tagsByTermOriginalForm.get(span.getStart() - 1);
-				//				Entry<String, String> tagsByTermAfterNP = (span.getEnd() == tagsByTermOriginalForm
-				//						.size()) ? null : tagsByTermOriginalForm.get(span
-				//						.getEnd());
-				//				List<Integer> indicesOfNameTerm = personNameIdentifier
-				//						.identifyNameTermsWithinNounPhrase(phraseBasicForm,
-				//								tagsByTermBeforeNP, tagsByTermAfterNP);
-				//				if (indicesOfNameTerm == null || indicesOfNameTerm.isEmpty())
-				//				{
-				//					continue;
-				//				}
-				//
-				//				List<Entry<String, String>> name = phraseBasicForm
-				//						.subList(indicesOfNameTerm.get(0), indicesOfNameTerm
-				//								.get(indicesOfNameTerm.size() - 1) + 1);
-				//				matchedFullNames
-				//						.add(new SimpleEntry<List<Entry<String, String>>, String>(
-				//								name, concatenateTerms(name)));
-				// System.out.println(name);
 			}
 
 			matchFullNames(tagsByTermOriginalForm, tagsByTermBasicForm,
@@ -196,7 +126,8 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 		List<Entry<List<Entry<String, String>>, String>> capitalizationsByOriginalCaseFromWiki = new ArrayList<Entry<List<Entry<String, String>>, String>>();
 		// single term
 		List<Entry<Entry<String, String>, String>> possibleCapitalizationsBySingleNounTermFromWiki = new ArrayList<Entry<Entry<String, String>, String>>();
-		findProperNounsInWikipedia(sentence, posTagsByTermOfSubSentences,
+		findProperNounsInWikipedia(sentence,
+				posTagsByOriginalCaseOfSubSentences,
 				capitalizationsByOriginalCaseFromWiki,
 				possibleCapitalizationsBySingleNounTermFromWiki);
 		LOGGER.info(">>>>> ProperNoun_wiki_sequence\t\t*"
@@ -206,26 +137,26 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 
 		// single noun from wikipedia is not reliable, 
 		// for example, "Fastest" is considered as a movie name which is capitalized; also "Become", "MCG", "Descendents"
-		List<Entry<Entry<String, String>, String>> capitalizationsBySingleNounTermFromWiki = filterOutNonProperSingleNounByWordNet(possibleCapitalizationsBySingleNounTermFromWiki);
-		LOGGER.info(">>>>> ProperNoun_wiki_single\t\t*"
+		// if the single term has been capitalized by true-caser, it is very likely to be correct
+		List<Entry<Entry<String, String>, String>> capitalizationsBySingleNounTermFromWiki = filterOutNotTrueCasedSingleTerm(
+				possibleCapitalizationsBySingleNounTermFromWiki,
+				originalPosTagsByTrueCaseOfSubSentences);
+		LOGGER.info(">>>>> True-cased_wiki_single\t\t*"
 				+ capitalizationsBySingleNounTermFromWiki);
-
-		//		// Get all noun sequences
-		//		List<List<Entry<String, String>>> nounSequences = findNounSequences(posTagsByTermOfSubSentences);
-		//		LOGGER.info(">>>>> Noun_from_parser\t\t\t" + nounSequences);
 
 		// Find those noun(sequence)s which have not been identified by Wikipedia
 		List<List<Entry<String, String>>> nounPhrasesNotIdentifiedByWiki = filterOutNounSequencesIdentifiedByWiki(
-		/*nounSequences*/allNounPhrases,
-				capitalizationsByOriginalCaseFromWiki,
+				allNounPhrases, capitalizationsByOriginalCaseFromWiki,
 				capitalizationsBySingleNounTermFromWiki);
 		LOGGER.info(">>>>> Noun_notInWiki\t\t\t"
 				+ nounPhrasesNotIdentifiedByWiki);
 
-		// Find the proper noun in WordNet
-		List<Entry<List<Entry<String, String>>, String>> capitalizationsByOriginalCaseFromWordNet = findProperNounsInWordNet(nounPhrasesNotIdentifiedByWiki);
-		LOGGER.info(">>>>> ProperNoun_notInWiki_inWN\t\t*"
-				+ capitalizationsByOriginalCaseFromWordNet);
+		// Find the proper noun in WordNet or true cased
+		List<Entry<List<Entry<String, String>>, String>> capitalizationsByOriginalCaseFromWordNetOrTrueCaser = findProperNounsInWordNetOrTrueCased(
+				nounPhrasesNotIdentifiedByWiki,
+				originalPosTagsByTrueCaseOfSubSentences);
+		LOGGER.info(">>>>> True-cased_notInWiki_inWN\t\t*"
+				+ capitalizationsByOriginalCaseFromWordNetOrTrueCaser);
 
 		/*
 		// TODO this matched single name is not used now, 
@@ -238,7 +169,7 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 		List<List<Entry<String, String>>> matchedNamesIdentifiedByNameListOnly = filterOutNamesByWikiAndWordNet(
 				matchedFullNames, capitalizationsByOriginalCaseFromWiki,
 				capitalizationsBySingleNounTermFromWiki,
-				capitalizationsByOriginalCaseFromWordNet);
+				capitalizationsByOriginalCaseFromWordNetOrTrueCaser);
 
 		LOGGER.info(">>>>> PersonName_notInWiki_notInWN\t*"
 				+ matchedNamesIdentifiedByNameListOnly);
@@ -263,7 +194,7 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 			sentence = sentence.replace(target, entry.getValue());
 		}
 
-		for (Entry<List<Entry<String, String>>, String> entry : capitalizationsByOriginalCaseFromWordNet)
+		for (Entry<List<Entry<String, String>>, String> entry : capitalizationsByOriginalCaseFromWordNetOrTrueCaser)
 		{
 			String target = concatenateTerms(entry.getKey());
 			sentence = sentence.replace(target, entry.getValue());
@@ -277,13 +208,6 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 		}
 
 		////////////////////////////////////////////////////////////////////////
-
-		//		// re-parse it /////////////////////////////////////////////////////////
-		//		posTagsByTerm = parseIntoPosTagByTerms(sentence);
-		//		// split by the punctuation
-		//		posTagsByTermOfSubSentences = splitByPunctuations(posTagsByTerm);
-		//		nounSequences = findNounSequences(posTagsByTermOfSubSentences);
-		//		LOGGER.info(">>>>> Re-parsed_Noun_from_parser\t\t" + nounSequences);
 
 		// fill the parameter nounPhrases with nounSequences
 		if (possibleNounPhrases != null)
@@ -304,6 +228,130 @@ public class HybridTrueCaserImpl extends AbstractTrueCaser
 		LOGGER.info("========================================================");
 
 		return sentence;
-		//return StringUtils.capitalize(sentence);
+	}
+
+	private List<Entry<Entry<String, String>, String>> filterOutNotTrueCasedSingleTerm(
+			List<Entry<Entry<String, String>, String>> possibleCapitalizationsBySingleNounTermFromWiki,
+			List<List<Entry<String, String>>> originalPosTagsByTrueCaseOfSubSentences)
+	{
+		List<Entry<Entry<String, String>, String>> result = new ArrayList<Entry<Entry<String, String>, String>>();
+
+		for (Entry<Entry<String, String>, String> entry : possibleCapitalizationsBySingleNounTermFromWiki)
+		{
+			String capitalizedTermInWiki = entry.getValue();
+			trueCased: for (List<Entry<String, String>> posByTrueCaseInSubSentence : originalPosTagsByTrueCaseOfSubSentences)
+			{
+				for (Entry<String, String> posByTrueCase : posByTrueCaseInSubSentence)
+				{
+					String trueCase = posByTrueCase.getKey();
+					if (trueCase.equals(capitalizedTermInWiki)) // the single term also capitalized by true-caser
+					{
+						result.add(entry);
+						break trueCased;
+					}
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private void parseIntoPosTagByOriginalAndTrueCase(String sentence,
+			List<Entry<String, String>> posTagsByOriginalCase,
+			List<Entry<String, String>> posTagsByTrueCase)
+	{
+		Annotation document = new Annotation(sentence);
+		getAnnotater().annotate(document);
+
+		for (CoreLabel token : document.get(TokensAnnotation.class))
+		{
+			String pos = token.get(PartOfSpeechAnnotation.class);
+			String originalCase = token.get(TextAnnotation.class);
+			String trueCase = token.get(TrueCaseTextAnnotation.class);
+
+			posTagsByOriginalCase.add(new SimpleEntry<String, String>(
+					originalCase, pos));
+			posTagsByTrueCase
+					.add(new SimpleEntry<String, String>(trueCase, pos));
+		}
+	}
+
+	private List<Entry<List<Entry<String, String>>, String>> findProperNounsInWordNetOrTrueCased(
+			List<List<Entry<String, String>>> posTagsByTermOfNounPhrases,
+			List<List<Entry<String, String>>> originalPosTagsByTrueCaseOfSubSentences)
+	{
+		List<Entry<List<Entry<String, String>>, String>> result = new ArrayList<Entry<List<Entry<String, String>>, String>>();
+
+		for (List<Entry<String, String>> nounSequence : posTagsByTermOfNounPhrases)
+		{
+			String trueCasedSequence = null;
+
+			if (nounSequence.size() > 1) // more than one term (i.e. phrase)
+			{
+				String nounSequenceString = concatenateTerms(nounSequence);
+				String nounSequenceInWordNet = wordNetReader
+						.retrieveTermInStandardCase(nounSequenceString,
+								POS.NOUN);
+				if (nounSequenceInWordNet != null
+						&& inDifferentCases(nounSequenceString,
+								nounSequenceInWordNet)) // matched in WN
+				{
+					result.add(new SimpleEntry<List<Entry<String, String>>, String>(
+							nounSequence, nounSequenceInWordNet));
+					continue;
+				}
+				//				else if ((trueCasedSequence = findCorrespondingTrueCasedSequence(
+				//						nounSequenceString,
+				//						originalPosTagsByTrueCaseOfSubSentences)) != null) // here we trust the true-caser
+				//				{
+				//					result.add(new SimpleEntry<List<Entry<String, String>>, String>(
+				//							nounSequence, trueCasedSequence));
+				//					continue;
+				//				}
+			}
+
+			// try each individual noun term
+			for (Entry<String, String> entry : nounSequence)
+			{
+				String term = entry.getKey().toLowerCase(Locale.US);
+				String termInWordNet = wordNetReader
+						.retrieveTermInStandardCase(term, POS.NOUN);
+				if (termInWordNet != null
+						&& inDifferentCases(term, termInWordNet) // exists capitalized term in WN
+						&& (trueCasedSequence = findCorrespondingTrueCasedSequence(
+								term, originalPosTagsByTrueCaseOfSubSentences)) != null) // and also be true cased
+				{
+					result.add(new SimpleEntry<List<Entry<String, String>>, String>(
+							Collections.singletonList(entry), trueCasedSequence));
+				}
+			}
+
+		}
+
+		return result;
+	}
+
+	private String findCorrespondingTrueCasedSequence(
+			String nounSequenceString,
+			List<List<Entry<String, String>>> originalPosTagsByTrueCaseOfSubSentences)
+	{
+		// nounSequenceString = nounSequenceString.toLowerCase(Locale.US); // already in lower case
+		for (List<Entry<String, String>> trueCasedSubSentence : originalPosTagsByTrueCaseOfSubSentences)
+		{
+			String trueCasedString = concatenateTerms(trueCasedSubSentence,
+					false, false); // MUST not in lower case!
+			int beginIndex = trueCasedString.toLowerCase(Locale.US).indexOf(
+					nounSequenceString);
+
+			if (!trueCasedString.contains(nounSequenceString)
+					&& beginIndex > -1) // contains the sequence but not in lower case
+			{
+				return trueCasedString.substring(beginIndex, beginIndex
+						+ nounSequenceString.length());
+			}
+
+		}
+
+		return null;
 	}
 }
